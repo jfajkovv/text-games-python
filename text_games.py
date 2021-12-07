25065626ed66cd23b5dfa0e3d76bbe9a6f99f7fa
Julian Fajkowski <jfajkovv@gmail.com>
2021-12-07 13:32:12 +0100

    create function that returns "y" or "n" in text_games.py module

--- text_games.py
+++ text_games.py
@@ -11,3 +11,12 @@ def ask_int_in_range(question, low, high):
     while reply not in range(low, high):
         reply = int(input('\t' + q))
     return reply
+
+
+def ask_yes_no(question):
+    '''Returns a "y" or "n" (as an answer to a question).'''
+
+    reply = None
+    while reply not in ('y', 'n'):
+        reply = input('\t' + q).lower()
+    return reply
