d6b56d4cecd5c18db5e3f43bba8c02e4cf23741f
Julian Fajkowski <jfajkovv@gmail.com>
2021-12-08 14:44:07 +0100

    refactor ask_int_in_range and ask_yes_no functions (params naming etc.)

--- text_games.py
+++ text_games.py
@@ -4,19 +4,19 @@
 ###
 ####
 
-def ask_int_in_range(question, low, high):
+def ask_int_in_range(user_query, range_bottom, range_top):
     '''Returns an integer (as an answer to a question).'''
 
     reply = None
-    while reply not in range(low, high):
-        reply = int(input('\t' + q))
+    while reply not in range(range_bottom, range_top + 1):
+        reply = int(input('\t' + user_query))
     return reply
 
 
-def ask_yes_no(question):
+def ask_yes_no(user_query):
     '''Returns a "y" or "n" (as an answer to a question).'''
 
     reply = None
     while reply not in ('y', 'n'):
-        reply = input('\t' + q).lower()
+        reply = input('\t' + user_query).lower()
     return reply
