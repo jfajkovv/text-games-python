d31d60b234129be550ca2ca7fad787793919f12f
Julian Fajkowski <jfajkovv@gmail.com>
2021-12-08 15:05:30 +0100

    create Card class in text_games.py module

--- text_games.py
+++ text_games.py
@@ -1,6 +1,6 @@
 ####
 ###
-### General utilities.
+### General utilities
 ###
 ####
 
@@ -20,3 +20,40 @@ def ask_yes_no(user_query):
     while reply not in ('y', 'n'):
         reply = input('\t' + user_query).lower()
     return reply
+
+####
+###
+### Card games
+###
+####
+
+class Card(object):
+    '''A standard game card.'''
+
+    SUITS = [
+        'c',  # clubs
+        'd',  # diamonds
+        'h',  # hearts
+        's',  # spades
+    ]
+
+    RANKS = [
+        'A',
+        '2', '3', '4', '5', '6', '7', '8', '9', '10',
+        'J', 'Q', 'K'
+    ]
+
+    def __init__(self, rank, suit, obverse_up=True):
+        self.rank = rank
+        self.suit = suit
+        self.is_obverse_up = obverse_up
+
+    def __str__(self):
+        if self.is_obverse_up:
+            re = f'[{self.rank}{self.suit}]'
+        else:
+            re = '[Xx]
+        return re
+
+    def flip(self):
+        self.is_obverse_up = not self.is_obverse_up
