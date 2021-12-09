cd46a1d85d617ecb77415faca97c21d96b1e999c
Julian Fajkowski <jfajkovv@gmail.com>
2021-12-09 22:44:35 +0100

    create Hand class in text_games.py module

--- text_games.py
+++ text_games.py
@@ -57,3 +57,34 @@ class Card(object):
 
     def flip(self):
         self.is_obverse_up = not self.is_obverse_up
+
+
+class Hand(object):
+    '''Collection of cards. A representation of player's game cards
+    - to play with at hand.'''
+
+    def __init__(self):
+        self.card_set = []
+
+    def __str__(self):
+        if self.card_set:
+            re = ''
+            for card in self.card_set:
+                re += str(card) + '\t'
+        else:
+            re = '<empty hand>'
+        return re
+
+    def stack_on(self, single_card):
+        self.card_set.append(single_card)
+
+    def give(self, a_card, other_hand):
+        self.card_set.remove(a_card)
+        other_hand.stack_on(single_card=a_card)
+
+    def transfer_all(self, route):
+        for card in self.card_set:
+            self.give(a_card=card, other_hand=route)
+
+    def clear(self):
+        self.card_set = []
